69c40653cb11e03d311a8a5e9f9227fe7fe5114e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-04-18 22:14:46 -0300

    criado o objeto despesa

--- js/app.js
+++ js/app.js
@@ -1,29 +1,56 @@
 class Despesa {
-    constructor( ano, mes, dia, tipo, descricao, valor) {
-        this.ano = ano
-        this.mes = mes
-        this.dia = dia
-        this.tipo = tipo
-        this.descriçao = descricao
-        this.valor = valor
-    }
+  constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+    this.tipo = tipo;
+    this.descriçao = descricao;
+    this.valor = valor;
+  }
 }
+
+class Bd {
+
+  constructor () {
+    let id = localStorage.getItem("id")
+
+    if(id === null) {
+      localStorage.setItem("id",0)
+    } 
+  }
+
+  getProximoId() { 
+    let proximoId = localStorage.getItem("id") // serve para recuperar o dado, nesse caso o retorno seria null
+    console.log(proximoId)
+  }
+
+  // class recebe um metodo gravar
+  gravar(d) {
+  //localStorage.setItem("despesa", JSON.stringify(d));
+  this.getProximoId
+  };
+}
+
+let bd = new Bd()
+
 
 function cadastrarDespesa() {
-  let ano =   document.getElementById('ano')
-  let mes =  document.getElementById('mes')
-  let dia =  document.getElementById('dia')
-  let tipo =  document.getElementById('tipo')
-  let descricao =  document.getElementById('descricao')
-  let valor =  document.getElementById('valor')
+  let ano = document.getElementById("ano");
+  let mes = document.getElementById("mes");
+  let dia = document.getElementById("dia");
+  let tipo = document.getElementById("tipo");
+  let descricao = document.getElementById("descricao");
+  let valor = document.getElementById("valor");
 
   let despesa = new Despesa(
-    ano.value, 
-    mes.value, 
-    dia.value, 
-    tipo.value, 
-    descricao.value, 
+    ano.value,
+    mes.value,
+    dia.value,
+    tipo.value,
+    descricao.value,
     valor.value
-  )
-  console.log(despesa)
+  );
+  bd.gravar(despesa);  // chamando o obhjeto bd e na sequencia executando o metodo gravar 
 }
+
+
